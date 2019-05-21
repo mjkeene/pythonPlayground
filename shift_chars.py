a056c76339b7d9bc301dc4061099b5eb89cead13
Michael Keene <mikekeene21@gmail.com>
2019-05-21 00:40:02 -0700

    Delete shift_chars.py

--- shift_chars.py
+++
@@ -1,7 +0,0 @@
-def shiftChars():
-    """A simple function that shifts the characters based on Unicode code."""
-    string = input('Enter a string and the characters will be shifted by 1. ')
-    newList = [chr(ord(i) + 1) for i in string]
-    string_adj = ''.join(newList)
-    print(string_adj)
-shiftChars()
